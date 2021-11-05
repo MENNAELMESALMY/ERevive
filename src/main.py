c9f25160c257d9d88cd365e81b7e1ea1e4215849
nadaabdelmaboud <nada.abdelmaboud@tactful.ai>
2021-11-05 23:42:10 +0200

    fix deleting paths

--- src/main.py
+++ src/main.py
@@ -71,7 +71,7 @@ print(dataTypesDic)
 
 connectedComponents,skeleton = connectEntities(scale_contours(finalContours[:],1.17),finalContours,binarizedImg,shapes,textArr)
 relations = get_relations(skeleton,connectedComponents)
-cardinality(relations,binarizedImg)
+cardinality(relations,skeleton)
 
 #connectEntities(finalContours,binarizedImg,shapes)
 
--- src/path_points.py
+++ src/path_points.py
@@ -6,7 +6,7 @@ from skimage.morphology import opening,erosion,dilation
 def enhance_contour(img,contour):
     empty_img = np.zeros(img.shape,np.uint8)
     cv.drawContours(empty_img, [contour], -1, 255,  cv.FILLED)
-    empty_img = opening(empty_img,np.ones((3,3),np.uint8))
+    empty_img = opening(empty_img,np.ones((1,1),np.uint8))
     actual_contour = np.where(empty_img==255)
     actual_contour = list(actual_contour)
     actual_contour.reverse()

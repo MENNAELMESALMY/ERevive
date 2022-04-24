f2946a76d2a3a4d108ada736f183ed804dc26138
Nihal-Mansour <nihalmansour0599@gmail.com>
2022-04-24 14:50:29 +0200

    card design 2 & 3

--- Interface_Vue/gpinterface/src/views/homeView.vue
+++ Interface_Vue/gpinterface/src/views/homeView.vue
@@ -6,6 +6,14 @@
     minim veni ullamco laboris nisi ut aliquip ex ea commodo consequat Excepteur
     sint occaecat cupidatat non proident"
     />
+    <cardDesign2
+      title="Lorem ipsum"
+      description="Lorem ipsum dolor sitUt enim ad laboris nisi ut aliquip ex ea commodo"
+    />
+    <cardDesign3
+      title="Lorem ipsum"
+      description="Lorem ipsum dolor sitUt enim ad laboris nisi ut aliquip ex ea commodo"
+    />
     <colorPallete />
   </div>
 </template>
@@ -19,10 +27,14 @@
 <script>
 import cardDesign from "../components/cardDesign.vue";
 import colorPallete from "../components/colorPallete.vue";
+import cardDesign2 from "../components/cardDesign2.vue";
+import cardDesign3 from "../components/cardDesign3.vue";
 export default {
   components: {
     cardDesign,
     colorPallete,
+    cardDesign2,
+    cardDesign3,
   },
 };
 </script>
